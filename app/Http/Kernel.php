9b381bcfd598e2915bf6e4e771e5983fd5e1e1b9
lolipopmari <mit.maria2019@yandex.ru>
2023-05-31 22:07:58 +0300

    feat: добавлены Middleware для ролей в системе

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'role.admin' => \App\Http\Middleware\AdminRoleMiddleware::class,
+        'role.student' => \App\Http\Middleware\StudentRoleMiddleware::class,
+        'role.teacher' => \App\Http\Middleware\TeacherRoleMiddleware::class,
     ];
 }
